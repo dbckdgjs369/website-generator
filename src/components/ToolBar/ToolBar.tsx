b8c1b35d654664261cfcbc2536b71310eca5796c
yoo chang heon <dbckdgjs1130@naver.com>
2023-10-22 15:30:35 +0900

    feat: 툴바에 새로운 기능 추가

--- src/components/ToolBar/ToolBar.tsx
+++ src/components/ToolBar/ToolBar.tsx
@@ -4,14 +4,25 @@ import { useGlobalEventEmitter } from "../../GlobalEventEmitterContext";
 export default function ToolBar() {
   const globalEmitter = useGlobalEventEmitter();
 
+  // 새로 생성된 div에 emitter가 중첩되는 문제발생
   const handleButton = (ev: React.MouseEvent<HTMLButtonElement>) => {
     ev.preventDefault();
     globalEmitter.emit("click", ev.currentTarget.id);
     globalEmitter.removeAllListeners();
   };
 
+  const addStyle = (ev: React.MouseEvent<HTMLButtonElement>) => {
+    ev.preventDefault();
+    globalEmitter.emit("style", ev.currentTarget.id);
+    globalEmitter.removeAllListeners();
+  };
+
   return (
     <S.ToolBarWrapper>
+      <S.CurrentWrapper>여기에 클릭한 요소가 보여야 됌</S.CurrentWrapper>
+      <S.CurrentWrapper>
+        <button onClick={addStyle}>스타일 추가</button>
+      </S.CurrentWrapper>
       <button onClick={(ev) => handleButton(ev)} id="div">
         div
       </button>
