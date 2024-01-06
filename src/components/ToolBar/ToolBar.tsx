fff4cd9d95e1699b037864ca1fa35fee4dc71bba
yoo chang heon <dbckdgjs1130@naver.com>
2024-01-06 17:24:56 +0900

    feat: 선택한 element의 스타일 세팅

--- src/components/ToolBar/ToolBar.tsx
+++ src/components/ToolBar/ToolBar.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import * as S from "./emotion";
 import { useGlobalEventEmitter } from "../../GlobalEventEmitterContext";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function ToolBar() {
+  const [elementStyle, setElementStyle] = useState<any>("");
   const globalEmitter = useGlobalEventEmitter();
   const textareaRef = useRef<HTMLTextAreaElement>(null);
 
@@ -24,6 +26,28 @@ export default function ToolBar() {
     globalEmitter.emit("file", ev.currentTarget?.id);
   };
 
+  const handleStyle = (style: any) => {
+    console.log(":::::style in toolbar", style);
+    if (!style) return;
+    const trimmedString = style.slice(1, -1);
+
+    // 큰따옴표 없애기
+    const withoutQuotes = trimmedString.replace(/"/g, "");
+
+    // 쉼표를 세미콜론으로 바꾸기
+    const cssProperties = withoutQuotes.replace(/,/g, ";\n") + ";";
+
+    setElementStyle(cssProperties);
+  };
+
+  useEffect(() => {
+    globalEmitter.on("client_style", handleStyle);
+
+    return () => {
+      globalEmitter.off("client_style", handleStyle);
+    };
+  }, []);
+
   return (
     <S.ToolBarWrapper>
       <S.CurrentWrapper>여기에 클릭한 요소가 보여야 됌</S.CurrentWrapper>
@@ -33,7 +57,12 @@ export default function ToolBar() {
         <S.Button>right</S.Button>
       </S.ButtonWrapper>
       <S.CurrentWrapper>
-        <textarea ref={textareaRef} />
+        <textarea
+          style={{ height: "200px" }}
+          ref={textareaRef}
+          value={elementStyle}
+          onChange={(ev) => setElementStyle(ev.target.value)}
+        />
         <button onClick={addStyle}>직접 스타일 추가</button>
       </S.CurrentWrapper>
       <button onClick={(ev) => handleButton(ev)} id="div">
