7fdca0ef364e16590017d5bb711172af81d497f2
yoo chang heon <dbckdgjs1130@naver.com>
2024-07-06 13:18:16 +0900

    feat: Provider 분리 및 컴포넌트 페이지 추가

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,11 +1,15 @@
 import { PropsWithChildren } from "react";
+
 import * as S from "./emotion";
 import ToolBar from "../components/ToolBar/ToolBar";
+import Provider from "../Provider";
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <S.Wrapper>
-      <S.MainView>{children}</S.MainView>
+      <Provider>
+        <S.MainView>{children}</S.MainView>
+      </Provider>
       <S.ToolBaWrapper>
         <ToolBar />
       </S.ToolBaWrapper>
