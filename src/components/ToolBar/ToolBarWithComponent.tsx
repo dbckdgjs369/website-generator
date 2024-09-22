626cdff6fdb2c3961a9efcf90d5009b11f4b4aa1
yoo chang heon <dbckdgjs1130@naver.com>
2024-09-22 23:51:22 +0900

    feat: save, load 기능 추가

--- src/components/ToolBar/ToolBarWithComponent.tsx
+++ src/components/ToolBar/ToolBarWithComponent.tsx
@@ -1,21 +1,21 @@
 import * as S from "./emotion";
 import { useGlobalEventEmitter } from "../../provider/GlobalEventProvider/GlobalEventEmitterContext";
-// import { allHtmlTags } from "../../constant/constant";
 
 export default function ToolBarWithComponent() {
   const globalEmitter = useGlobalEventEmitter();
 
-  const handleSave = () => {
-    const currentPage = document.documentElement.outerHTML;
-    console.log("::current", currentPage);
+  const handleFile = (type: "save" | "load") => {
+    globalEmitter.emit("file", type);
   };
+
   const handleButton = (ev: React.MouseEvent<HTMLButtonElement>) => {
     ev.preventDefault();
     globalEmitter.emit("add", ev.currentTarget.id);
   };
   return (
     <S.ToolBarWrapper>
-      <button onClick={handleSave}>save</button>
+      <button onClick={() => handleFile("save")}>save</button>
+      <button onClick={() => handleFile("load")}>load</button>
       <button onClick={(ev) => handleButton(ev)} id={"input"} key={"input"}>
         input
       </button>
--- src/layer/ComponentLayer.tsx
+++ src/layer/ComponentLayer.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { isEmpty } from "lodash";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
@@ -9,8 +8,7 @@ import { useGlobalEventEmitter } from "../provider/GlobalEventProvider/GlobalEve
 import { HTMLTag } from "../types";
 import { generateRandomString } from "../utils/utils";
 import useRender2 from "../hooks/useRender2";
-// import { createRoot } from "react-dom/client";
-// import DraggableComponent from "../components/DraggableComponent";
+import { Position } from "../hooks/useRender";
 
 export default function Componentlayer({
   children,
@@ -23,7 +21,6 @@ export default function Componentlayer({
   const currentPageName = location.pathname.split("component/")[1] ?? "main";
   const { pageData, updatePageData: setPageData } =
     usePageData(currentPageName);
-  console.log(":pageData", pageData);
 
   const { parseElementsToHTML } = useRender2();
   const { getElementById } = useHandleStructure();
@@ -36,13 +33,11 @@ export default function Componentlayer({
   };
 
   const handleAddElement = (name: HTMLTag) => {
-    console.log("::::add");
     // addElement(name, selectedID);
     addElement(name, "default");
   };
 
   const addElement = (addObjectTag: HTMLTag, targetObjectID: string) => {
-    console.log(":::addObject", addObjectTag, targetObjectID);
     // id를 가진 요소에 object Tag를 추가해줌
     if (isEmpty(pageData)) return;
     const updatedPageData = [...pageData];
@@ -52,17 +47,13 @@ export default function Componentlayer({
       foundObject.inner.push({
         id: generateRandomString(10),
         type: addObjectTag,
-        // style: DEFAULT_STYLE,
-        // text: addObjectTag,
         root: true,
       });
-      // foundObject.root = true;
     } else {
       foundObject.inner = [
         {
           id: generateRandomString(10),
           type: addObjectTag,
-          // style: DEFAULT_STYLE,
           text: addObjectTag,
           root: true,
         },
@@ -70,51 +61,53 @@ export default function Componentlayer({
     }
     setPageData(updatedPageData);
   };
-  console.log("pageData", pageData);
 
-  const handlePosition = (pos: any, id: string) => {
-    console.log(":::layer", pos, id);
+  const handlePosition = (pos: Position, id: string) => {
     if (isEmpty(pageData)) return;
     const updatedPageData = [...pageData];
     const foundObject = getElementById(updatedPageData, id);
-    console.log(":::foundObject", foundObject, id);
     if (pos) {
       const e = document.getElementById(id);
-      console.log("::handlePosition", e);
       if (e) {
-        console.log("position", `translate(${pos.x}px,${pos.y}px)`);
         e.style["transform"] = `translate(${pos.x}px,${pos.y}px)`;
       }
     }
     if (!foundObject) return;
     foundObject.position = pos;
 
-    // styleArr.forEach((style: string) => {
-    //   if (style.length === 0) return;
-    //   const row = style.split(":");
-    //   newStyleObject[row[0]] = row[1];
-    // });
-    // transform: translate(700px, 200px);
-
-    // globalEmitter.emit("element_style", JSON.stringify({ ...newStyleObject }));
-
     setPageData(updatedPageData);
   };
-  useEffect(() => {
-    globalEmitter.on("test", handlePosition);
-  }, [globalEmitter]);
+  const handleFile = (name: string) => {
+    switch (name) {
+      case "save": {
+        localStorage.setItem("pageJson", JSON.stringify(pageData));
+        break;
+      }
+      case "load": {
+        const loadData = localStorage.getItem("pageJson");
+        if (loadData) {
+          const loadedData = JSON.parse(loadData);
+          setPageData(loadedData);
+        }
+        break;
+      }
+    }
+  };
+
   useEffect(() => {
     if (isEmpty(pageData)) return;
     globalEmitter.on("add", handleAddElement);
-    globalEmitter.on("test", handlePosition);
+    globalEmitter.on("position", handlePosition);
+    globalEmitter.on("file", handleFile);
+
     getSelectedElementInfo(selectedID);
     return () => {
       globalEmitter.off("add", handleAddElement);
-      globalEmitter.off("test", handlePosition);
+      globalEmitter.off("position", handlePosition);
+      globalEmitter.off("file", handleFile);
     };
   }, [globalEmitter, selectedID, pageData, currentPageName]);
 
-  console.log("::selectedID", selectedID);
   const getID = (ev: React.MouseEvent<HTMLDivElement>) => {
     if (ev.target instanceof Element) {
       const id = ev.target.id;
