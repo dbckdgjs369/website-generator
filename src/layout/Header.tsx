1960ab19b1248a07222d9039c4957200f3ce21ab
yoo chang heon <dbckdgjs1130@naver.com>
2024-10-13 15:39:34 +0900

    feat: Header 아바타 추가

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,24 +1,26 @@
-import { useNavigate } from "react-router-dom";
-import { Button, Row } from "groot-component-library";
+import { useNavigate, useParams } from "react-router-dom";
+import { Avatar, Button, Row } from "groot-component-library";
 
 import * as S from "./emotion";
+import Img from "../assets/groot.png";
 import { useGlobalEventEmitter } from "../provider/GlobalEventProvider/GlobalEventEmitterContext";
 
 export default function Header() {
   const navigate = useNavigate();
   const globalEmitter = useGlobalEventEmitter();
+  const { id } = useParams();
 
   const handleFile = (type: "save" | "load") => {
     globalEmitter.emit("file", type);
   };
   return (
     <S.Header tag={"header"}>
-      <div style={{ border: "1px solid", width: "20px", height: "20px" }}></div>
+      <Avatar src={Img} size={30} />
       <Row style={{ gap: "10px" }}>
         <Button
           backgroundColor="#ffc107"
           style={{ height: "30px" }}
-          onClick={() => navigate("/result")}
+          onClick={() => navigate(`/result/${id}`)}
         >
           result
         </Button>
